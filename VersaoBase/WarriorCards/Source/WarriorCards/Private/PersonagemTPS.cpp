1c7c71e86ca6550856f7ed8d6c48d6509db9edab
Bruno Dias de Amorim <brunodyas@icloud.com>
2023-02-19 14:25:21 -0300

    Feat: Adicao de cartas e Mapa

---
+++ VersaoBase/WarriorCards/Source/WarriorCards/Private/PersonagemTPS.cpp
@@ -0,0 +1,96 @@
+#include "PersonagemTPS.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Character.h"
+
+// Construtor
+APersonagemTPS::APersonagemTPS()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Setup Camera
+	SpringArmCamera = CreateDefaultSubobject<USpringArmComponent>(FName("SpringArmCamera"));
+	SpringArmCamera->TargetArmLength = 200.f;
+	SpringArmCamera->bUsePawnControlRotation = true;
+	SpringArmCamera->AddRelativeLocation(FVector(0.f,40.f,50.f));
+	SpringArmCamera->SetupAttachment(RootComponent);
+
+	CameraPersonagem = CreateDefaultSubobject<UCameraComponent>(FName("CameraPersonagem"));
+	CameraPersonagem->SetupAttachment(SpringArmCamera);
+
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+
+	// Auto possess
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	GetCharacterMovement()->AirControl = 0.05f;
+	GetCharacterMovement()->JumpZVelocity = 425.f;
+	GetCharacterMovement()->GravityScale = 1.5f;
+	
+}
+
+// Called when the game starts or when spawned
+void APersonagemTPS::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APersonagemTPS::MoverParaFrente(float Valor)
+{
+	AddMovementInput(GetActorForwardVector() * Valor);
+}
+
+void APersonagemTPS::MoverDireita(float Valor)
+{
+	AddMovementInput(GetActorRightVector() * Valor);
+}
+
+void APersonagemTPS::Pular()
+{
+	bEstaPulando = true;
+}
+
+void APersonagemTPS::PararPulo()
+{
+	bEstaPulando = false;
+}
+
+void APersonagemTPS::Agachar()
+{
+	Crouch();
+}
+
+void APersonagemTPS::Levantar()
+{
+	UnCrouch();
+}
+
+// Called every frame
+void APersonagemTPS::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APersonagemTPS::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoverParaFrente", this, &APersonagemTPS::MoverParaFrente);
+	PlayerInputComponent->BindAxis("MoverDireita", this, &APersonagemTPS::MoverDireita);
+	PlayerInputComponent->BindAxis("OlharCimaBaixo", this, &APersonagemTPS::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("OlharEsquerdaDireita", this, &APersonagemTPS::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Agachar", EInputEvent::IE_Pressed, this, &APersonagemTPS::Agachar);
+	PlayerInputComponent->BindAction("Agachar", EInputEvent::IE_Released, this, &APersonagemTPS::Levantar);
+
+	PlayerInputComponent->BindAction("Pular", EInputEvent::IE_Pressed, this, &APersonagemTPS::Pular);
+	PlayerInputComponent->BindAction("Pular", EInputEvent::IE_Released, this, &APersonagemTPS::PararPulo);
+}	
+
